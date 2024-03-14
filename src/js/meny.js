967589e2d70c6f85a40619bbd4f5bf77950b5f4a
Mica Soderlund <120316501+mica-soderlund@users.noreply.github.com>
2024-03-14 15:03:06 +0100

    färdiga updates mobilnav scss_layout, html och js

--- src/js/meny.js
+++ src/js/meny.js
@@ -1,9 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+//MOBILMENY hamburger 
+document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.getElementById("mobile-menu-icon");
     const mobileMenu = document.getElementById("mobile-menu");
+    const closeButton = document.getElementById("close-menu"); // Hämta "Stäng"-knappen
   
-    menuIcon.addEventListener("click", function() {
-      mobileMenu.style.transform = mobileMenu.style.transform === "translateX(100%)" ? "translateX(0)" : "translateX(100%)";
+    menuIcon.addEventListener("click", () => {
+      mobileMenu.style.transform = "translateX(0)"; // Öppnar 
+    });
+  
+    closeButton.addEventListener("click", () => {
+      mobileMenu.style.transform = "translateX(100%)"; // Stänger 
     });
   });
   
