49272975e7f4d06e4b94dfb5ab17583989f96229
Mica Soderlund <120316501+mica-soderlund@users.noreply.github.com>
2024-03-14 14:28:26 +0100

    justeringar nav-meny + animation planet

--- src/js/meny.js
+++ src/js/meny.js
@@ -1,38 +1,9 @@
-//HAMBURGARE-MENY MOBIL
-document.addEventListener('DOMContentLoaded', function() {
-    const burgerButton = document.querySelector('.burger-button');
-    const mobileMenu = document.querySelector('.mobile-menu-items');
-
-    function toggleMenu() {
-        burgerButton.classList.toggle('active');
-        mobileMenu.classList.toggle('active');
-
-        // Lägger till eller tar bort eventlyssnare baserat på om menyn är aktiv eller inte
-        if (mobileMenu.classList.contains('active')) {
-            document.addEventListener('click', closeMenuOnClickOutside);
-        } else {
-            document.removeEventListener('click', closeMenuOnClickOutside);
-        }
-    }
-
-    function closeMenuOnClickOutside(event) {
-        // Kontrollerar om klicket är utanför menyn och knappen
-        if (!burgerButton.contains(event.target) && !mobileMenu.contains(event.target)) {
-            burgerButton.classList.remove('active');
-            mobileMenu.classList.remove('active');
-            // Tar bort eventlyssnaren eftersom menyn nu är stängd
-            document.removeEventListener('click', closeMenuOnClickOutside);
-        }
-    }
-
-    if (burgerButton && mobileMenu) {
-        burgerButton.addEventListener('click', function(event) {
-            // Förhindrar att dokumentets click-event omedelbart stänger menyn
-            event.stopPropagation();
-            toggleMenu();
-        });
-    } else {
-        console.error("Mobile meny was not found.");
-    }
-});
-
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("mobile-menu-icon");
+    const mobileMenu = document.getElementById("mobile-menu");
+  
+    menuIcon.addEventListener("click", function() {
+      mobileMenu.style.transform = mobileMenu.style.transform === "translateX(100%)" ? "translateX(0)" : "translateX(100%)";
+    });
+  });
+  
